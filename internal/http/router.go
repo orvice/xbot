d51f408ff2b52a6567d82dcdf36bc27265c3f8ea
orvice L <orvice@gmail.com>
2025-02-19 23:05:31 +0800

    log

--- internal/http/router.go
+++ internal/http/router.go
@@ -2,11 +2,19 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"butterfly.orx.me/core/log"
 	"go.orx.me/xbot/internal/bot"
 )
 
 func Router(m *gin.Engine) {
 	m.Any("/v1/webhook", func(c *gin.Context) {
+		logger := log.FromContext(c.Request.Context())
+		logger.Info("new webhook request",
+			"header", c.Request.Header,
+			"method", c.Request.Method,
+			"uri", c.Request.RequestURI,
+		)
 		bot.GetBot().WebhookHandler().ServeHTTP(c.Writer, c.Request)
 	})
 }
