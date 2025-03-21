75c7cd975c2d259811c00270de7604b9ab77b374
orvice <orvice@gmail.com>
2025-03-21 17:20:13 +0800

    update gen image

--- internal/pkg/openai/client.go
+++ internal/pkg/openai/client.go
@@ -70,8 +70,8 @@ func GenImage(ctx context.Context, promptString string) (string, error) {
 		N:              1,
 		Model:          conf.Conf.PictureVendor.Model,
 		Size:           openai.CreateImageSize1792x1024,
-		Quality:        openai.CreateImageQualityHD,
-		Style:          openai.CreateImageStyleNatural,
+		// Quality:        openai.CreateImageQualityHD,
+		Style: openai.CreateImageStyleNatural,
 	}
 	resp, err := pictureClient.CreateImage(ctx, reqBase64)
 	if err != nil {
